e8f7bbc1b58cec0b7ad8a85c68bc5f622d10ebf7
Shashank <shashankkolle2006@gmail.com>
2026-05-22 19:09:10 +0530

    Add files via upload

---
+++ Voting-DApp-Fresh/contracts/Voting.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Voting {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+
+    uint public candidatesCount;
+
+    constructor() {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _id) public {
+        require(!voters[msg.sender], "Already voted");
+        require(_id > 0 && _id <= candidatesCount);
+
+        voters[msg.sender] = true;
+        candidates[_id].voteCount++;
+    }
+}
